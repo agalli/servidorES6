5e0f280a3a0ed7179dee802a49ac0472e3a61b37
Alejandro Galli <alejandro.galli@globant.com>
2016-07-25 15:19:33 -0300

    agregado class user y jasmine

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import express from 'express';
 import auth from 'basic-auth';
+import {user} from './lib/user';
 
 const app=express();
 
 let validar_usuario=(req, res, next)=> {
   console.log("antes de el get");
-  let user=auth(req);
-  console.log('el nombre %s el pass %s', user.name, user.pass);
-  next();
+  let u=auth(req);
+  let usuario=new user(u.name, u.pass)
+  console.log('el nombre %s el pass %s', usuario.nombre, usuario.pass);
+  if(usuario.isValid()){
+    next();
+  }else{
+    res.send('usuario invalido');
+  }
 }
 
 
